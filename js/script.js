c402e5f466b43cfde6986da00b20c0e8533b4912
geo2arc <ggeorgala@gmail.com>
2016-03-04 01:13:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,6 +33,51 @@ $(".facet-title").click(function() {
 		}
 })
 
+/*--------------------------------------------------------------
+# Filters: FacetWP Infinite Scroll (fitya)
+--------------------------------------------------------------*/
+
+$('#loaded').hide();
+
+window.fwp_is_paging = false;
+
+$(document).on('facetwp-refresh', function() {
+	if (! window.fwp_is_paging) {
+		window.fwp_page = 1;
+		FWP.extras.per_page = 'default';
+	}
+
+	window.fwp_is_paging = false;
+});
+
+$(document).on('facetwp-loaded', function() {
+	window.fwp_total_rows = FWP.settings.pager.total_rows;
+
+	if (! FWP.loaded) {
+		window.fwp_default_per_page = FWP.settings.pager.per_page;
+
+		$(window).scroll(function() {
+			if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+				var rows_loaded = (window.fwp_page * window.fwp_default_per_page);
+				if (rows_loaded < window.fwp_total_rows) {
+					window.fwp_page++;
+					window.fwp_is_paging = true;
+					FWP.extras.per_page = (window.fwp_page * window.fwp_default_per_page);
+					FWP.soft_refresh = true;
+					$('#loading').show();
+					setInterval(function() {$('#loading').hide();}, 4000);
+					FWP.refresh();
+					
+				} else { $('#loaded').show(); }
+			}
+		});
+	}
+});
+
+/*
+usage: after content create divs #loading, #loaded style="display:none;"
+reference: https://facetwp.com/infinite-scroll-with-facetwp/
+**/
 
 
 }); //jQuery
