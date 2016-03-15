e20c5191dd8cf0fd9e728290f061a946a27c07ba
geo2arc <ggeorgala@gmail.com>
2016-03-15 02:51:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -65,52 +65,6 @@ $(document).on('facetwp-loaded', function() {
 	$('.facetwp-depth .facetwp-depth').prev().addClass('has-children');
 });
 
-/*--------------------------------------------------------------
-FILTERS: Custom Pagination (ecodomisi, tgc)
---------------------------------------------------------------*/
 
-function custom_facetwp_pager( $output, $params ) {
-    $output = '';
-    $page = (int) $params['page'];
-    $total_pages = (int) $params['total_pages'];
-	$output .= '<ul class="uk-pagination page-numbers">';
-    // Only show pagination when > 1 page
-    if ( 1 < $total_pages ) {
-        if ( 1 < $page ) {
-            $output .= '<li><a class="facetwp-page" data-page="' . ( $page - 1 ) . '">&laquo; Previous</a></li>';
-        }
-        if ( 3 < $page ) {
-            $output .= '<li><a class="facetwp-page first-page" data-page="1">1</a></li>';
-            $output .= ' <span class="dots">…</span> ';
-        }
-        for ( $i = 2; $i > 0; $i-- ) {
-            if ( 0 < ( $page - $i ) ) {
-                $output .= '<li><a class="facetwp-page" data-page="' . ($page - $i) . '">' . ($page - $i) . '</a></li>';
-            }
-        }
-        // Current page
-        $output .= '<li class="uk-active"><span class="facetwp-page active current"><a style="padding:0" data-page="' . $page . '">' . $page . '</a></span></li>';
-        for ( $i = 1; $i <= 4; $i++ ) {
-            if ( $total_pages >= ( $page + $i ) ) {
-                $output .= '<li><a class="facetwp-page" data-page="' . ($page + $i) . '">' . ($page + $i) . '</a></li>';
-            }
-        }
-        if ( $total_pages > ( $page + 4 ) ) {
-            $output .= ' <li><span class="dots">…</span></li> ';
-            $output .= '<li><a class="facetwp-page last-page" data-page="' . $total_pages . '">' . $total_pages . '</a></li>';
-        }
-        if ( $page < $total_pages ) {
-            $output .= '<li><a class="facetwp-page" data-page="' . ( $page + 1 ) . '">Next &raquo;</a></li>';
-        }
-    }
-	
-	$output .="</ul>";
-    return $output;
-}
-add_filter( 'facetwp_pager_html', 'custom_facetwp_pager', 10, 2 );
-
-/*
-reference: Custom pagination (functions.php) – https://gist.github.com/mgibbs189/9732174
-**/
 
 }); //jQuery
